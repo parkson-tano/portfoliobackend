e30f0841117154551864e6832c93c0a15e3dcd21
asbilim <bilimdepaulilian@gmail.com>
2023-07-19 04:13:23 +0100

    postgresql database configured

--- camerfolio/settings.py
+++ camerfolio/settings.py
@@ -63,11 +63,22 @@ WSGI_APPLICATION = 'camerfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'asbilim',
+    'PASSWORD': 'd8MziEN4qCLf',
+    'HOST': 'ep-super-snow-812319.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
+
+# To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
+
 
 
 # Password validation
